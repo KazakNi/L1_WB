c0f0888beaa2c7bae2dd3ea0a82d3edca8071bda
Nikita Kazakov <kazakov.ni@yandex.ru>
2024-03-08 00:47:22 +0300

    task 13 has been completed

--- task_13/main.go
+++ task_13/main.go
@@ -1,8 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"sync/atomic"
+)
+
 /* Поменять местами два числа без создания временной переменной.
  */
 
 func main() {
+	// first variant
+	var a atomic.Int32
+	a.Store(1)
+	var b int32 = 2
+	b = a.Swap(b)
+	fmt.Printf("a = %v, b = %v\n", a.Load(), b)
 
+	// second variant
+
+	c, d := 2, 3
+	c, d = d, c
+	fmt.Printf("c = %v, d = %v\n", c, d)
+
+	// third variant behind the second variant
+
+	c = d ^ c
+	d = c ^ d
+	c = d ^ c
+	fmt.Printf("c = %v, d = %v", c, d)
 }
